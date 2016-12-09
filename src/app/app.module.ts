448b157a715c4e7c348d21419d2d4b3fdc76f21c
Sebastiaan <sebastiaan@Sebastiaans-MacBook-Pro.local>
2016-12-09 18:09:22 +0100

    added material-design styling module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './components/core/app.component';
 import { EnergyUsageOverviewComponent } from './components/energy-usage-overview/energy-usage-overview.component';
@@ -17,7 +18,8 @@ import { MapperService } from './services/mapper-service/mapper.service';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule.forRoot()
   ],
   providers: [
     DataService,
--- src/app/components/energy-usage-overview/energy-usage-overview.component.ts
+++ src/app/components/energy-usage-overview/energy-usage-overview.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-energy-usage-overview',
   templateUrl: './energy-usage-overview.component.html',
-  styleUrls: ['./energy-usage-overview.component.css']
+  styleUrls: ['./energy-usage-overview.component.scss']
 })
 export class EnergyUsageOverviewComponent implements OnInit {
 
