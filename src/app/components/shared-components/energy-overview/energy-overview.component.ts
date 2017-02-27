a60e7ea9b8777caf1225447ebc404588adf9f34b
Sebastiaan <sebastiaan@Sebastiaans-MacBook-Pro.local>
2017-02-27 21:30:16 +0100

    added part energy-overview component and implemented mapperservice

--- src/app/components/shared-components/energy-overview/energy-overview.component.ts
+++ src/app/components/shared-components/energy-overview/energy-overview.component.ts
@@ -1,8 +1,12 @@
-import { Component, OnInit, AfterViewInit, ElementRef, ViewChild} from '@angular/core';
+import { Component, OnInit, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 import { Constants } from './constants';
 import { DataService } from '../../../services/data-service/data.service';
+import { MapperService } from '../../../services/mapper-service/mapper.service';
+
 import { Subscription } from 'rxjs';
 import * as d3 from 'd3';
+import * as moment from 'moment';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-energy-overview',
@@ -13,10 +17,9 @@ import * as d3 from 'd3';
 export class EnergyOverviewComponent implements OnInit, AfterViewInit {
   container;
   visualization;
-  // startDay = moment().subtract(2, 'days').format("YYYY-MM-DD");
-  // endDay = moment().format("YYYY-MM-DD");
+  // startDay = moment().subtract(2, 'days').format('YYYY-MM-DD');
+  // endDay = moment().format('YYYY-MM-DD');
   dataSubscription: Subscription;
-  data;
 
   xScale;
   yScale;
@@ -28,7 +31,7 @@ export class EnergyOverviewComponent implements OnInit, AfterViewInit {
   visualizationContainer: ElementRef;
 
 
-  constructor(private C: Constants, private dataService: DataService) {
+  constructor(private C: Constants, private dataService: DataService, private mapperService: MapperService) {
     // TypeScript + ES6 dependency injection
   }
 
@@ -42,23 +45,74 @@ export class EnergyOverviewComponent implements OnInit, AfterViewInit {
       .attr('preserveAspectRatio', 'xMidYMid meet')
       .attr('viewBox', '0 0 900 550'); // TODO: viewbox still necessary?
 
-    this.setup();
+    this.visualization = this.container.append('g');
+    this.loadData();
   }
 
-  private setup(): void {
-    // let minDate = data.wind_energy[0].date,
-    //             maxDate = data.wind_energy[data.wind_energy.length - 1].date;
-
-    //         xScale = d3.time.scale().domain([minDate, maxDate]).range([MARGINS.left, WIDTH - MARGINS.right]);
-
-    this.loadDataAndDrawGraph();
-  }
-
-  private loadDataAndDrawGraph(): void {
+  private loadData(): void {
     this.dataSubscription = this.dataService.getOverviewData()
       .subscribe(data => {
-        this.data = data;
+        let transformedData = this.mapperService.transformdata(data);
+        this.setupGraphLayout(transformedData);
+        this.drawGraph(transformedData);
       });
+  }
+
+  private setupGraphLayout(data): void {
+    // get min and max dates - this assumes data is sorted
+    let minDate = data.wind_energy[0].date;
+    let maxDate = data.wind_energy[data.wind_energy.length - 1].date;
+
+    this.xScale = d3.scaleTime().domain([minDate, maxDate]).range([this.C.MARGINS.left, this.C.WIDTH - this.C.MARGINS.right]);
+
+    // create an array of all values so that a min and max value can be determined
+    // todo: find more efficient way
+    let minMaxData = [];
+    for (let key of data) {
+      minMaxData.push.apply(minMaxData, _.map(data[key], 'value'));
+    }
+
+    this.yScale = d3.scaleLinear().range([this.C.HEIGHT - this.C.MARGINS.bottom, this.C.MARGINS.top])
+      .domain([0, d3.max(minMaxData) + 100]); // %
+
+    // create rectangular background (filled with grey color)
+    this.visualization.append('rect')
+      .attr('class', 'background');
+    // create x-axis
+    this.xAxis = d3.axisBottom(this.xScale);
+    // create y-axis
+    this.yAxis = d3.axisLeft(this.yScale);
+
+    // Add title
+    // this.visualization.append('svg:text')
+    //   .attr('class', 'graph-title')
+    //   .attr('x', this.C.MARGINS.left)
+    //   .attr('y', this.C.MARGINS.top / 2)
+    //   .text('Energy Active Dashboard');
+
+    // add y-axis label
+    this.visualization.append('text')
+      .attr('class', 'axis-label y-axis-label')
+      .attr('text-anchor', 'end')
+      .attr('transform', 'rotate(-90)')
+      .attr('y', this.C.MARGINS.left - 70)
+      .attr('x', -((this.C.HEIGHT / 2) - this.C.MARGINS.bottom))
+      .attr('dy', '.75em')
+      .text('Energy in Watts');
+
+    // add x-axis label
+    let dateToday = moment().format('dddd, MMMM Do YYYY');
+    this.visualization.append('text')
+      .attr('class', 'axis-label x-axis-label')
+      .attr('text-anchor', 'end')
+      .attr('y', this.C.HEIGHT - (this.C.MARGINS.bottom / 2) + 20)
+      .attr('x', this.C.WIDTH / 2 + this.C.MARGINS.left)
+      .attr('dy', '.75em')
+      .text(dateToday);
+  }
+
+  private drawGraph(data): void {
+
   }
 
 }
--- src/app/services/mapper-service/mapper.service.ts
+++ src/app/services/mapper-service/mapper.service.ts
@@ -1,9 +1,74 @@
 import { Injectable } from '@angular/core';
+import * as _ from 'lodash';
 
 @Injectable()
 export class MapperService {
 
   constructor() { }
 
-  mapData(): void {} // stub
+  transformdata(data): MappedData {
+    let consumptionData = data.consumptionData.ms;
+    let windData = data.windData.ms;
+
+    // Sort and map one array with objects belonging to a certain type of 'origin'
+    let mappedDataWind = _.chain(windData)
+      .map(function (item) {
+        return { origin: 'wind-energy', value: +item.v, date: new Date(item.ts) };
+      })
+      .sortBy(function (item) {
+        return item.date;
+      })
+      .value();
+
+    let mappedDataConsumption = _.chain(consumptionData)
+      .map(function (item) {
+        let modifiedItem = item.v >= 0 ? item.v : 0;
+        return { origin: 'energy-consumption', value: +modifiedItem, date: new Date(item.ts) };
+      })
+      .sortBy(function (item) {
+        return item.date;
+      })
+      .value();
+
+    let mappedDataSunOverSupply = _.chain(consumptionData)
+      .map(function (item) {
+        let modifiedItem = item.v >= 0 ? 0 : Math.abs(item.v);
+        return { origin: 'sun-energy', value: +modifiedItem, date: new Date(item.ts) };
+      })
+      .sortBy(function (item) {
+        return item.date;
+      })
+      .value();
+
+    let greenEnergy = _.chain(mappedDataWind)
+      .map(function (itemWind, i) {
+        let correspondingSunEnergyItemValue = mappedDataSunOverSupply[i].value;
+        return {
+          origin: 'green-energy',
+          value: +itemWind.value + correspondingSunEnergyItemValue,
+          date: itemWind.date
+        };
+      })
+      .sortBy(function (item) {
+        return item.date;
+      })
+      .value();
+
+    // then create an object of all the mapped data and return this to the calling function
+    let mappedData = new MappedData();
+
+    mappedData.wind_energy = mappedDataWind;
+    mappedData.green_energy = greenEnergy;
+    mappedData.sun_energy = mappedDataSunOverSupply;
+    mappedData.energy_consumption = mappedDataConsumption;
+
+    return mappedData;
+  }
 }
+
+ class MappedData {
+    wind_energy: any;
+    green_energy: any;
+    sun_energy: any;
+    energy_consumption: any;
+};
